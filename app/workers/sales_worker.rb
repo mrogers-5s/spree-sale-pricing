b30ad39a2a5190be8b528eaac1fa8f2d029ab24d
dangerdogz <fboivin63@gmail.com>
2015-11-23 14:21:36 -0500

    Add percent off

--- app/workers/sales_worker.rb
+++ app/workers/sales_worker.rb
@@ -2,20 +2,21 @@ class SalesWorker
   include Sidekiq::Worker
   def perform(chunk)
 
+    #group = Spree::SaleGroup.find(group_id)
+
     ActiveRecord::Base.transaction do
       chunk.each do |row|
-
-        puts"##########################################"
-        puts "#{row.inspect}"
-        puts"##########################################"
 
         product = Spree::Product.find_by(product_code: row['product_id'])
 
         if product
-          puts"##########################################"
-          puts product.inspect
-          puts"##########################################"
-          product.put_on_sale row['sale_price']
+          if row['sale_type'] && row['sale_type'] == 'percent' && row['sale_price'] < 1
+            @sale_price = product.put_on_sale row['sale_price'], "Spree::Calculator::PercentOffSalePriceCalculator"
+          else
+            @sale_price = product.put_on_sale row['sale_price']
+          end
+
+          #group.sale_prices << @sale_price
         else
           puts "PRODUCT NOT FOUND"
         end
