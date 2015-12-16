d35f10d67933d9d182ca1c6e2ade596fa344eae6
dangerdogz <fboivin63@gmail.com>
2015-12-16 11:16:22 -0500

    fix null values

--- app/workers/sales_worker.rb
+++ app/workers/sales_worker.rb
@@ -13,12 +13,9 @@ class SalesWorker
           puts "#############################"
           puts row.inspect
           puts "#############################"
-          if row['sale_type'] && row['sale_type'] == 'percent' && row['sale_price'] < 1
-            @sale_price = product.put_on_sale row['sale_price'], { calculator_type: Spree::Calculator::PercentOffSalePriceCalculator.new }
-          else
+          if row['sale_price'] > 1 && row['start_date'] && row['product_id']
             @sale_price = product.put_on_sale row['sale_price'], { start_at: row['start_date'], end_at: row['end_date']}
           end
-          #group.sale_prices << @sale_price
         else
           puts row.inspect
         end
@@ -27,4 +24,26 @@ class SalesWorker
 
     end
   end
+end
+
+#$LAST
+Spree::Product.find_each do |record|
+  translation = record.translation_for(I18n.default_locale) || record.translations.build(:locale => I18n.default_locale)
+    translation[:product_code] = record.read_attribute(:product_code, {:translated => false})
+  translation.save!
+end
+
+all_translated_attributes = Spree::Product.all.collect{|m| m.attributes}
+
+#FIRST
+all_translated_attributes.each do |translated_record|
+  # Create a hash containing the translated column names and their values.
+  [:product_code].inject(fields_to_update={}) do |f, name|
+    f.update({name.to_sym => translated_record[name.to_s]})
+  end
+
+  # Now, update the actual model's record with the hash.
+
+  Spree::Product.where(:id => translated_record['id']).update_all(fields_to_update)
+
 end
