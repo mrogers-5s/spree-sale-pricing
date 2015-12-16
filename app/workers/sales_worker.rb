1736237cb1b1755b77448e5b002c1565efc97d3a
dangerdogz <fboivin63@gmail.com>
2015-12-16 16:07:39 -0500

    what the fuck was this

--- app/workers/sales_worker.rb
+++ app/workers/sales_worker.rb
@@ -26,24 +26,3 @@ class SalesWorker
   end
 end
 
-#$LAST
-Spree::Product.find_each do |record|
-  translation = record.translation_for(I18n.default_locale) || record.translations.build(:locale => I18n.default_locale)
-    translation[:product_code] = record.read_attribute(:product_code, {:translated => false})
-  translation.save!
-end
-
-all_translated_attributes = Spree::Product.all.collect{|m| m.attributes}
-
-#FIRST
-all_translated_attributes.each do |translated_record|
-  # Create a hash containing the translated column names and their values.
-  [:product_code].inject(fields_to_update={}) do |f, name|
-    f.update({name.to_sym => translated_record[name.to_s]})
-  end
-
-  # Now, update the actual model's record with the hash.
-
-  Spree::Product.where(:id => translated_record['id']).update_all(fields_to_update)
-
-end
