30f4c18fe06b06dfd2135260ddf3eab7fe29beab
dangerdogz <fboivin63@gmail.com>
2016-04-08 16:00:04 -0400

    hide menu if you're not supposed to see it

--- app/overrides/add_import_sales_to_men_bar.rb
+++ app/overrides/add_import_sales_to_men_bar.rb
@@ -2,5 +2,7 @@ Deface::Override.new(:virtual_path=>'spree/admin/shared/_main_menu',
                      :name => 'add_import_sale_to_men_bar',
                      :insert_after =>"erb[silent]:contains('current_store')~erb[silent]",
                      :text         =>"<ul class='nav nav-sidebar'>
+                                       <% if can? :admin, Spree::SalePrice %>
                                         <%= main_menu_tree 'Soldes', icon: 'gift', sub_menu: 'spree_sales', url: '#sidebar-sales' %>
+                                      <% end %>
                                      </ul>")
